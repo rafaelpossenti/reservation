6fc6a1f0c87b4e477bb846e415f5305fffc8822b
rafael.possenti <rafael41051010>
2021-11-23 15:47:40 -0300

    add swagger doc

--- src/main/java/com/possenti/reservation/controller/ReservationController.java
+++ src/main/java/com/possenti/reservation/controller/ReservationController.java
@@ -3,14 +3,17 @@ package com.possenti.reservation.controller;
 import com.possenti.reservation.dto.ReservationCodeDto;
 import com.possenti.reservation.dto.ReservationDto;
 import com.possenti.reservation.service.ReservationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,13 +23,17 @@ import javax.validation.Valid;
 import java.time.LocalDate;
 import java.util.List;
 
+
 @RestController
 @RequestMapping("/reservations")
+@Tag(name = "Reservation API", description = "Campsite Reservation API Contract")
 public class ReservationController {
 
     @Autowired
     private ReservationService reservationService;
 
+    @Operation(summary = "Provide information of the availability of the campsite for a given date range with the default being 1 month.",
+            responses = {@ApiResponse(responseCode = "200", description = "Success")})
     @GetMapping("/available-dates")
     public ResponseEntity<List<LocalDate>> getAvailableDates(
             @RequestParam(value = "arrivalDate", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate arrivalDate,
@@ -35,6 +42,9 @@ public class ReservationController {
         return ResponseEntity.ok(availableDays);
     }
 
+    @Operation(summary = "Create new reservations", responses = {
+            @ApiResponse(responseCode = "200", description = "Success"),
+            @ApiResponse(responseCode = "400", description = "Bad request")})
     @PostMapping
     public ResponseEntity<ReservationCodeDto> create(
             @RequestBody @Valid ReservationDto reservationDto
@@ -43,15 +53,23 @@ public class ReservationController {
         return ResponseEntity.ok(reservationCodeDto);
     }
 
-    @PatchMapping("/{reservation_id}")
+    @Operation(summary = "Update an existent reservation", responses = {
+            @ApiResponse(responseCode = "200", description = "Success"),
+            @ApiResponse(responseCode = "404", description = "Not found"),
+            @ApiResponse(responseCode = "400", description = "Bad request")})
+    @PutMapping("/{reservation_id}")
     public ResponseEntity<ReservationCodeDto> update(@RequestBody ReservationDto dto,
                                                      @PathVariable("reservation_id") String reservationId) {
         final ReservationCodeDto reservationCodeDto = reservationService.update(reservationId, dto);
         return ResponseEntity.ok(reservationCodeDto);
     }
 
+    @Operation(summary = "Cancel an existent reservation", responses = {
+            @ApiResponse(responseCode = "204", description = "Success"),
+            @ApiResponse(responseCode = "404", description = "Not found")})
     @DeleteMapping("/{reservation_id}")
     public void cancel(@PathVariable("reservation_id") String reservationId) {
         reservationService.delete(reservationId);
+        ResponseEntity.noContent().build();
     }
 }
